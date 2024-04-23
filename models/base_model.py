c6da01c6235d18460d28c1514649bb924742b689
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-23 15:46:48 +0000

    Upaded '__init__'

--- models/base_model.py
+++ models/base_model.py
@@ -2,17 +2,33 @@
 """The base module"""
 import uuid
 import datetime
-
+    
 
 class BaseModel():
     """The base model class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization method"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key in kwargs.keys():
+                if key == "__class__":
+                    pass
+                elif key == "created_at" or key == "updated_at":
+                    value = datetime.datetime.fromisoformat(kwargs[f'{key}'])
+                    self.__dict__[f'{key}'] = value
+                else:
+                    self.__dict__[f'{key}'] = kwargs[f'{key}']
+            if 'id' not in kwargs.keys():
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs.keys():
+                self.created_at = datetime.datetime.now()
+            if 'updated_at' not in kwargs.keys():
+                self.updated_at = self.created_at
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Overriden __str__ method"""
@@ -27,8 +43,10 @@ class BaseModel():
     def to_dict(self):
         """Returning a dictionary representation"""
 
-        dict_repr = self.__dict__
+        dict_repr = {}
+        for key in self.__dict__.keys():
+            dict_repr[f'{key}'] = self.__dict__[f'{key}']
         dict_repr['created_at'] = self.created_at.isoformat()
         dict_repr['updated_at'] = self.updated_at.isoformat()
-        dict_repr['__class__'] = self.__class__.__name__
+        dict_repr['__class__'] = f'{self.__class__.__name__}'
         return dict_repr
