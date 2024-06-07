83a43c82940376c291138554176ba6a0371dc0a8
Ahmad <ahmad.new.m.v@gmail.com>
2024-06-07 10:12:29 +0000

    Small improvements

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,15 +18,31 @@ class FileStorage():
 
     def new(self, obj):
         """Adding a new object to "__objects" """
+        from models.base_model import BaseModel
+        from models.place import Place
+        from models.amenity import Amenity
+        from models.city import City
+        from models.review import Review
+        from models.state import State
+        from models.user import User
+
+        classes = {'BaseModel': BaseModel, 'Place': Place,
+                   'Amenity': Amenity, 'City': City,
+                   'Review': Review, 'State': State,
+                   'User': User}
 
         obj_dict = obj.to_dict()
-        FileStorage.__objects[f'{obj.__class__.__name__}.{obj.id}'] = obj_dict
+        k = f'{obj.__class__.__name__}.{obj.id}'
+        FileStorage.__objects[k] = classes[obj.__class__.__name__](**obj_dict)
 
     def save(self):
         """Serializing "__objects" to the JSON file in "__file_path" """
 
         with open(FileStorage.__file_path, "w", encoding="utf-8") as json_file:
-            json_file.write(json.dumps(FileStorage.__objects))
+            obj_dict = {}
+            for key, value in FileStorage.__objects.items():
+                obj_dict.update({key: value.to_dict()})
+            json_file.write(json.dumps(obj_dict))
 
     def reload(self):
         """Desirializing the contents of the file in "__file_path" into
@@ -35,5 +51,20 @@ class FileStorage():
         if os.path.isfile(FileStorage.__file_path):
             file_path = FileStorage.__file_path
             with open(file_path, "r", encoding="utf-8") as json_file:
+                from models.base_model import BaseModel
+                from models.place import Place
+                from models.amenity import Amenity
+                from models.city import City
+                from models.review import Review
+                from models.state import State
+                from models.user import User
+
+                classes = {'BaseModel': BaseModel, 'Place': Place,
+                           'Amenity': Amenity, 'City': City,
+                           'Review': Review, 'State': State,
+                           'User': User}
                 file_content = json_file.read()
-                FileStorage.__objects = json.loads(file_content)
+                dict_content = json.loads(file_content)
+                for k, obj_repr in dict_content.items():
+                    cls = k.split('.')[0]
+                    FileStorage.__objects.update({k: classes[cls](**obj_repr)})
